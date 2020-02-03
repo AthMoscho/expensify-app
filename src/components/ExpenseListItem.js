edf9990134e34d20ec804fafd60ad0aa79ef8906
Thanos Mosh <ath.moschogiannis@gmail.com>
2020-02-03 12:56:43 +0200

    Updating ExpenseListItem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
+numeral.register('locale', 'gr', {
+    delimiters: {
+        thousands: ',',
+        decimal: '.'
+    },
+    currency: {
+        symbol: '€'
+    }
+})
+
+numeral.locale('gr');
 
 const ExpenseListItem = ({id, description, amount, createdAt }) => (
     <div>
         <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {numeral(amount / 100).format('$0,0.00')}
+             -  
+            {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
     </div>
 );
 
