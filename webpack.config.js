12d75752da631aeec611e7b9a9084bfb1da863d8
Thanos Mosh <ath.moschogiannis@gmail.com>
2020-02-03 10:53:14 +0200

    Updating Production Webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,53 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {      
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname,'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    console.log('env',env)
+    return {
+        plugins: [new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        })],      
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname,'public'),
+            filename: 'bundle.js'
         },
-        {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    }
+                    ,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
+        ]
+        },
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname,'public'),
+            historyApiFallback: true
+    
         }
-    ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname,'public'),
-        historyApiFallback: true
-
     }
-};
+}
