c338ddc3c66f4a8003deb026c064153bc8acfac5
Thanos Mosh <ath.moschogiannis@gmail.com>
2020-02-04 10:13:21 +0200

    Adding Sum Header

--- src/components/ExpensesDashboardPage.js
+++ src/components/ExpensesDashboardPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
-import ExpenseListItem from './ExpenseListItem';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpensesDashboardPage = () => (
     <div>
+        <ExpensesSummary />
         <ExpenseListFilters />
         <ExpenseList />
     </div>
